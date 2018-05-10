4bfd6f889f25e809bbe3c7ef0153bf8f45c405c2
iMac <imac@iMacs-iMac.local>
2018-05-10 21:18:00 +0600

    complete the project

--- Hustle-mode/ViewController.swift
+++ Hustle-mode/ViewController.swift
@@ -7,18 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var darkBlue: UIImageView!
+    @IBOutlet var powerBtn: UIButton!
+    @IBOutlet var cloudHolder: UIView!
+    @IBOutlet var rocket: UIImageView!
+    @IBOutlet var cloud: UIImageView!
+    @IBOutlet var hustlLbl: UILabel!
+    @IBOutlet var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }catch let error as NSError{
+            print(error.description)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func Power(_ sender: UIButton) {
+        darkBlue.isHidden = true
+        powerBtn.isHidden = true
+        cloudHolder.isHidden = false
+        
+        player.play()
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 220, width: 375, height: 144)
+        }) { (finished) in
+            self.hustlLbl.isHidden = false
+            self.onLbl.isHidden = false
+        }
     }
+    
 
 
 }
